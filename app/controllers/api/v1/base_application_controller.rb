3ff5f7be521606a7e99c6cf9cc0e74d6fd96f538
斎藤正大 <saima.ptw88@gmail.com>
2020-11-14 23:17:16 +0900

    テンプレートコントローラーにテスト実装

--- app/controllers/api/v1/base_application_controller.rb
+++ app/controllers/api/v1/base_application_controller.rb
@@ -1,8 +1,7 @@
-module Api
-  module V1
-    class BaseApplicationController < ApplicationController
-      include DeviseTokenAuth::Concerns::SetUserByToken
-      protect_from_forgery with: :null_session
-    end
-  end
+class Api::V1::BaseApplicationController < ApplicationController
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # userがサインインしていない場合、401エラーを返す
+  before_action :authenticate_api_v1_user!
+
+  protect_from_forgery with: :null_session
 end
--- spec/requests/api/v1/template_request_spec.rb
+++ spec/requests/api/v1/template_request_spec.rb
@@ -1,11 +1,216 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Templates", type: :request do
+  # index
   describe "GET /api/v1/template" do
-    subject { get(api_v1_template_index_path) }
+    subject { get(api_v1_template_index_path, headers: headers) }
 
-    it "returns http success" do
-      # expect{ subject }.to have_http_status(:success)
+    let(:headers) { @user.create_new_auth_token }
+    before do
+      @user = create(:user)
+      create_list(:template, 3)
+      3.times { @user.templates.create(title: Faker::Internet.domain_word, body: Faker::Commerce.department) }
+
+      @array = []
+      @tem = Template.all
+      @tem.each do |i|
+        if i[:user_id] == @user.id
+          @array.push(i[:user_id])
+        end
+      end
+    end
+
+    it "レスポンスが正常" do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "ユーザーに紐づいたテンプレートが返ってくる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.map {|x| x["user_id"] }).to eq @array
+    end
+  end
+
+  # show
+  describe "GET /api/v1/template/:id" do
+    subject { get(api_v1_template_path(template_id), headers: headers) }
+
+    before {  @user = create(:user) }
+
+    let(:template_id) { @template.id }
+    let(:headers) { @user.create_new_auth_token }
+
+    context "ユーザーに紐づいたテンプレートである場合" do
+      before do
+        @template = @user.templates.create!(title: Faker::Internet.domain_word, body: Faker::Commerce.department)
+        create_list(:template, 3)
+        create_list(:template, 3, user_id: @user.id)
+      end
+
+      it "正常なレスポンスを返す" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "ログインユーザーと同じuser_idのテンプレートを取得" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["user_id"]).to eq @user.id
+      end
+
+      it "指定したidのテンプレートを取得" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["id"]).to eq @template.id
+      end
+
+      it "1つのテンプレートを取得" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.count).to eq 6
+      end
+    end
+
+    context "ユーザーに紐づいていないテンプレートの場合" do
+      before { @template = create(:template, user_id: user.id) }
+
+      let(:user) { create(:user) }
+
+      it "エラー" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  # create
+  describe "POST /api/v1/template" do
+    subject { post(api_v1_template_index_path, params: params, headers: headers) }
+
+    let(:headers) { @user.create_new_auth_token }
+    let(:res) { JSON.parse(response.body) }
+
+    before { @user = create(:user) }
+
+    context "適切なパラメータを送信する" do
+      let(:params) { { template: attributes_for(:template) } }
+
+      it "レスポンスが正常" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "正常にテンプレートが保存される" do
+        expect { subject }.to change { Template.count }.by(1)
+      end
+
+      it "titleが正常" do
+        subject
+        expect(res["title"]).to eq params[:template][:title]
+      end
+
+      it "bodyが正常" do
+        subject
+        expect(res["body"]).to eq params[:template][:body]
+      end
+    end
+
+    context "正常なパラメータを送信しない場合" do
+      let(:params) { { template: attributes_for(:template, title: "") } }
+
+      it "エラー" do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  # update
+  describe "PUTCH, PUT /api/v1/template/:id" do
+    subject { put(api_v1_template_path(template_id), params: params, headers: headers) }
+
+    before do
+      @user = create(:user)
+      @template = create(:template, user_id: @user.id)
+    end
+
+    let(:template_id) { @template.id }
+    let(:headers) { @user.create_new_auth_token }
+    let(:res) { JSON.parse(response.body) }
+
+    context "正常なパラメータを送信した場合" do
+      context "title, body共に送信した場合" do
+        let(:params) { { template: attributes_for(:template) } }
+
+        it "レスポンスが正常" do
+          subject
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context "titleのみ送信した場合" do
+        let(:params) { { template: { title: Faker::Internet.domain_word } } }
+
+        it "titleが更新される" do
+          subject
+          expect(res["title"]).to eq params[:template][:title]
+        end
+
+        it "bodyが変更されない" do
+          subject
+          expect(res["body"]).to eq @template.body
+        end
+      end
+
+      context "bodyのみ送信した場合" do
+        let(:params) { { template: { body: Faker::Commerce.department } } }
+
+        it "titleは更新されない" do
+          subject
+          expect(res["title"]).to eq @template.title
+        end
+
+        it "bodyは更新される" do
+          subject
+          expect(res["body"]).to eq params[:template][:body]
+        end
+      end
+    end
+
+    context "正常でないパラメータを送信した場合" do
+      let(:params) { { template: { id: "123" } } }
+
+      it "title変化なし" do
+        subject
+        expect(res["title"]).to eq @template.title
+      end
+
+      it "body変化なし" do
+        subject
+        expect(res["body"]).to eq @template.body
+      end
+
+      it "id変化なし" do
+        subject
+        expect(res["id"]).to eq @template.id
+      end
+    end
+  end
+
+  # destroy
+  describe "DELETE /api/v1/template/:id" do
+    subject { delete(api_v1_template_path(template_id), headers: headers) }
+
+    before do
+      @user = create(:user)
+      @template = create(:template, user_id: @user.id)
+      create_list(:template, 3)
+    end
+
+    let(:template_id) { @template.id }
+    let(:headers) { @user.create_new_auth_token }
+
+    it "対象が削除される" do
+      expect { subject }.to change { Template.count }.by(-1)
     end
   end
 end
