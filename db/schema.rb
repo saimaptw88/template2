66aac9a3dc273dbc07b181422a30160403c22312
斎藤正大 <saima.ptw88@gmail.com>
2020-11-14 15:52:20 +0900

    bundle exec rails g migration AddUserIdToTemplateusings template:referencesを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_053847) do
+ActiveRecord::Schema.define(version: 2020_11_14_064933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_11_14_053847) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "template_id", default: 1, null: false
+    t.index ["template_id"], name: "index_templateusings_on_template_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_11_14_053847) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "templateusings", "templates"
 end
--- spec/models/templateusing_spec.rb
+++ spec/models/templateusing_spec.rb
@@ -2,11 +2,20 @@
 #
 # Table name: templateusings
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  body        :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  template_id :bigint           default(1), not null
+#
+# Indexes
+#
+#  index_templateusings_on_template_id  (template_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (template_id => templates.id)
 #
 require "rails_helper"
 
