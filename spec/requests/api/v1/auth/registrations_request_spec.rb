b541d1cc3b8b6e86202b61e47b4beb3b11ebc644
斎藤正大 <saima.ptw88@gmail.com>
2020-11-17 20:44:00 +0900

    retuching agein

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "POST api/v1/auth/registrations #create" do
     subject { post(api_v1_user_registration_path, params: params) }
 
-    let(:res){ JSON.parse(response.body) }
+    let(:res) { JSON.parse(response.body) }
 
     context "正常な値を送信した場合" do
-      let(:params){ attributes_for(:user) }
+      let(:params) { attributes_for(:user) }
 
       it "レスポンスが正常" do
         subject
@@ -20,27 +20,27 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
 
       it "nameが登録される" do
         subject
-        expect( res["data"]["name"] ).to eq params[:name]
+        expect(res["data"]["name"]).to eq params[:name]
       end
 
       it "emailが登録される" do
         subject
-        expect( res["data"]["email"] ).to eq params[:email]
+        expect(res["data"]["email"]).to eq params[:email]
       end
     end
 
     context "正常でない値を送信した場合" do
       context "passwordが空の場合" do
-        let(:params){ { :name => "saito", :email => "saito@sample.com" } }
+        let(:params) { { name: "saito", email: "saito@sample.com" } }
 
-          it "登録に失敗する" do
-            subject
-            expect(res["errors"]["password"]).to eq ["can't be blank"]
-          end
+        it "登録に失敗する" do
+          subject
+          expect(res["errors"]["password"]).to eq ["can't be blank"]
+        end
       end
 
       context "emailが空の場合" do
-        let(:params){ { :name => "saito", :password => "123456" } }
+        let(:params) { { name: "saito", password: "123456" } }
 
         it "登録に失敗する" do
           subject
