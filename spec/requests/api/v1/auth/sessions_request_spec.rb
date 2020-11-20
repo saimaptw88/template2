6d47d9df86e02cb1616c80dc974ddfa4283e02ae
斎藤正大 <saima.ptw88@gmail.com>
2020-11-20 21:18:43 +0900

    logout機能実装

--- spec/requests/api/v1/auth/sessions_request_spec.rb
+++ spec/requests/api/v1/auth/sessions_request_spec.rb
@@ -35,4 +35,42 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
       end
     end
   end
+
+  describe "DELETE /api/v1/auth/sign_out" do
+    subject { delete(destroy_api_v1_user_session_path, headers: headers) }
+
+    let(:headers) { user.create_new_auth_token }
+    let(:user) { create(:user) }
+    let(:res) { JSON.parse(response.body) }
+
+    context "正常な値を送信した場合" do
+      it "レスポンスが正常" do
+        subject
+        expect(response).to have_http_status :ok
+      end
+
+      it "tokenを削除しログアウトできる" do
+        subject
+        expect(user.reload.tokens).to be_blank
+      end
+    end
+
+    context "誤った情報を送信した時" do
+      let(:user) { create(:user) }
+      let(:headers) do
+        {
+          "access-token" => "",
+          "token-type" => "",
+          "client" => "",
+          "expiry" => "",
+          "uid" => "",
+        }
+      end
+      it "ログインに失敗する" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["success"]).to eq false
+      end
+    end
+  end
 end
